5a8edb5ac6c08c50a99f64d4ff471b9a0388c392
dacer <qqwerrd@gmail.com>
2013-10-11 19:24:35 +0900

    add

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,131 @@
-//auto set body height = browser view height
-$("#main").css("height", $(window).height() - $("footer").outerHeight( true ));
+
+    // console.log(id);
+var input = [];
+var currencyRate;
+var supportTouch = ('ontouchstart' in document.documentElement);
+
+initListener();
+
+function initListener(){
+  for (var i = 11; i >= 0; i--) {
+    var id = "#num-"+i;
+    $(id).on("touchstart", onTouchStart(i));
+    $(id).on("touchend", onTouchEnd(i));
+    if (!supportTouch) {
+      $(id).on("click", onClickMe(i));
+    }
+  };
+  $("#btn-back").on("click", backAnimation());
+
+  $("#to-conversion").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#conventer-div").offset().top
+    }, 800);
+  });
+
+  $("#btn-back").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#show-div").offset().top
+    }, 800);
+  });
+}
+
+function onTouchStart(num){
+  return function() {
+      $("#num-"+num).css("backgroundColor", "rgba(255,255,255,1)");
+      $("#num-"+num).css("color", "rgba(0,0,0,1)");
+    };
+}
+
+function onTouchEnd(num){
+  return function() {
+      $("#num-"+num).css("backgroundColor", "rgba(0,0,0,0)");
+      $("#num-"+num).css("color", "rgba(255,255,255,1)");
+      if (supportTouch) {
+        onClickOrigin(num);
+      }
+    };
+}
+
+function onClickMe(num){
+  return function(){onClickOrigin(num)};
+}
+
+function onClickOrigin(num){
+  if(num === 10){
+    deleteInput();
+  }else if (num === 11) {
+
+  }else{
+    addInput(num);
+  };
+}
+function addInput(num){
+  input.push(num);
+  refreshInput();
+}
+function deleteInput(){
+  input.pop();
+  refreshInput();
+}
+
+function refreshInput(){
+  var resultInput = "";
+  $.each(input, function( index, value ) {
+    resultInput += value;
+  });
+  $("#first-rate").text(resultInput);
+
+  var floatResult =  parseFloat(resultInput);
+
+  $("#second-rate").text(floatResult*2);
+}
+
+function backAnimation(){
+  return function(){
+    $('#main-div').animate({"marginTop":"250px"}, 500, function() {
+      $('#main-div').animate({"marginTop":"-880px"},888,function(){
+        // location.reload();
+        alert("Enter!");
+      });
+    });
+  }
+}
+
+function loadingAnimate(){
+  $('#loading-div').css("display","block"); 
+}
+
+function stopLoading(){
+  $('#loading-div').css("display","none");
+}
+
+
+//http://stackoverflow.com/questions/7837456/comparing-two-arrays-in-javascript
+// attach the .compare method to Array's prototype to call it on any array
+Array.prototype.compare = function (array) {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0; i < this.length; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].compare(array[i]))
+                return false;
+        }
+        else if (this[i] != array[i]) {
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;
+        }
+    }
+    return true;
+}
 
 
 
